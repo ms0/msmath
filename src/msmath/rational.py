c258aa1dfab96fa259bb1fea777ae4f89e07e603
Mike Speciner <ms@alum.mit.edu>
2023-06-14 15:12:29 -0400

    Add numerator and denominator for x and qrational
    denominator is lcm of component denominators
    numerator is self*denominator

--- src/msmath/rational.py
+++ src/msmath/rational.py
@@ -16,7 +16,7 @@ import sys
 
 from math import log as _mlog, modf as _mmodf, ldexp as _mldexp, copysign as _mcopysign
 from itertools import chain, count
-from . conversions import xrange, isint, isstr, gcd, bit_length
+from . conversions import xrange, isint, isstr, gcd, bit_length, lcm, lcma
 from . quaternion import quaternion
 
 if sys.version_info[0] < 3 :
@@ -880,6 +880,8 @@ class xrational(object) :
 Instance variables (read only):
   real: the real part, a rational
   imag: the imaginary part, a rational
+  denominator: the lcm of the denominators of real and imag
+  numerator: denominator*self
 Methods:
   __new__, __hash__, __repr__, __str__, bstr, __bool__, __nonzero__,
   __eq__, __ne__, __lt__, __le__, __ge__, __gt__,
@@ -953,6 +955,16 @@ If real is a string (and imag==0), return xrational(rational(real))"""
   def imag(self) :
     """the imaginary part"""
     return self._b;
+
+  @property
+  def denominator(self) :
+    """the lcm of real and imag denominators"""
+    return lcm(self._a._b,self._b._b);
+
+  @property
+  def numerator(self) :
+    """self * self.denominator"""
+    return self*self.denominator;
 
   def __eq__(self,other) :
     """Return True iff self == other"""
@@ -1249,6 +1261,8 @@ Instance variables (read only):
    i,j,k: the respective components of the vector part, each a rational
    imag: the imaginary part, but only if j and k components are zero
    sv or rv: (s,i,j,k), a tuple of rationals
+   denominator: the lcm of the denominators of sv
+   numerator: denominator*self
 Methods:
   __new__, __hash__, __repr__, __str__, bstr, __bool__, __nonzero__,
   __eq__, __ne__, __lt__, __le__, __ge__, __gt__,
@@ -1393,6 +1407,16 @@ If four args, the quaternion args[0] + i*args[1] + j*args[2] + k*args[3] is retu
   def v(self) :
     """the vector part as a tuple (i,j,k)"""
     return self.__v[1:];
+
+  @property
+  def denominator(self) :
+    """the lcm of the component's denominators"""
+    return lcma(map(lambda x:x._b, self.__v));
+
+  @property
+  def numerator(self) :
+    """self * self.denominator"""
+    return self*self.denominator;
 
   def __bool__(self) :
     return any(self.__v);
@@ -2165,11 +2189,14 @@ def fmod(x,y) :
   return rational(x)%y;
 
 def floor(x) :
-  """Return the largest integer not greater than x, or x if not finite"""
+  """Return the largest integer not greater than x, or x if not finite;
+but if x is not real but complex, return the nearest Gaussian integer;
+but if x is not complex but quaternion, return the nearest Hurwitz integer"""
   return rational(x)//1;
 
 def ceil(x) :
-  """Return the smallest integer not less than x, or x if not finite"""
+  """Return the smallest integer not less than x, or x if not finite;
+  but if x is not real, return -floor(-x)"""
   return -(rational(-x)//1);
 
 def hypot(*x) :
